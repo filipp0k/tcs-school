88627b2f5dd680aa1e149860c1b6f0fd6b45e874
Филипп Дюдин <Filipp0k@macbook-pro-filipp.local>
2017-02-20 21:27:39 +0300

    Initial Commit

---
+++ tcs-school/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tcs-school
+//
+//  Created by Филипп Дюдин on 20.02.17.
+//  Copyright © 2017 Филипп Дюдин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
