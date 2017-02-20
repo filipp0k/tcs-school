a4e3b76e850900eefe9a42c6d05ddbf605703a14
Филипп Дюдин <Filipp0k@macbook-pro-filipp.local>
2017-02-21 00:51:11 +0300

    Added all elements

--- tcs-school/ViewController.swift
+++ tcs-school/ViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    let currencies = ["RUB", "USD", "EUR"]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencies.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencies[row]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.label.text = "Здесь будет курс"
+        
+        self.pickerFrom.dataSource = self
+        self.pickerTo.dataSource = self
+        self.pickerFrom.delegate = self
+        self.pickerTo.delegate = self
     }
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var pickerFrom: UIPickerView!
-    
     @IBOutlet weak var pickerTo: UIPickerView!
     @IBOutlet weak var label: UILabel!
 
